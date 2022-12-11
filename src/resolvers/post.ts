310d66c88113ac61fb598b58bc313e68f03caf63
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-12-11 11:25:11 +0100

    CRUD with apollo & mikro-orm

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,44 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() ctx: MyContext): Promise<Post[]> {
     return ctx.em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg("id", () => Int) id: number, @Ctx() ctx: MyContext): Promise<Post | null> {
+    return ctx.em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(@Arg("title") title: string, @Ctx() ctx: MyContext): Promise<Post> {
+    const post = ctx.em.create(Post, { title });
+    await ctx.em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(@Arg("id") id: number, @Arg("title") title: string, @Ctx() ctx: MyContext): Promise<Post | null> {
+    const post = await ctx.em.findOne(Post, { id });
+    if (!post) return null;
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await ctx.em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(@Arg("id") id: number, @Ctx() ctx: MyContext): Promise<boolean> {
+    try {
+      await ctx.em.nativeDelete(Post, { id });
+      return true;
+    } catch (error) {
+      return false;
+    }
   }
 }
