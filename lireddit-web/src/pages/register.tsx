ff5f3d056a92cf879f95264e678506c2767963ce
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-12-17 13:56:43 +0100

    Formik errors when gql returns some

--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -3,20 +3,25 @@ import React from "react";
 import { Form, Formik } from "formik";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
-import { useMutation } from "urql";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface RegisterProps {}
 
 export const Register: React.FC<RegisterProps> = ({}) => {
   const [, register] = useRegisterMutation();
 
+  const router = useRouter();
+
   return (
     <Wrapper variant='small'>
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
+          if (response.data?.register.errors) setErrors(toErrorMap(response.data.register.errors));
+          else if (response.data?.register.user) router.push("/");
         }}>
         {({ isSubmitting }) => (
           <Form>
