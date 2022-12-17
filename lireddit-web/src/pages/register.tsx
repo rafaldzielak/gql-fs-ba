5fd7a2021ec5f092bd2929b4cab6ed26bbfd98da
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-12-17 13:13:27 +0100

    FE: Register mutation

--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -1,16 +1,39 @@
-import { FormControl, FormLabel, Input, FormErrorMessage, Button, Box } from "@chakra-ui/react";
+import { Button, Box } from "@chakra-ui/react";
 import React from "react";
-import { Field } from "type-graphql";
 import { Form, Formik } from "formik";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
+import { useMutation } from "urql";
 
 interface RegisterProps {}
 
+const REGISTER_MUTATION = `
+mutation Register($username: String!, $password: String! ){
+  register(options: {password: $password username:$username})   {
+    errors {
+      field
+      message
+    }
+    user{
+      createdAt
+      id
+      updatedAt
+      username
+    }
+  }
+}`;
+
 export const Register: React.FC<RegisterProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUTATION);
+
   return (
     <Wrapper variant='small'>
-      <Formik initialValues={{ username: "", password: "" }} onSubmit={(values) => console.log(values)}>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+          return register(values);
+        }}>
         {({ isSubmitting }) => (
           <Form>
             <Box mt='4'>
