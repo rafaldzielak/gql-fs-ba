772a634b5d31d63d90c77a04f139d8f776140a9a
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-01-24 20:38:10 +0100

    FE: voteStatus

--- lireddit-web/src/components/UpdootSection.tsx
+++ lireddit-web/src/components/UpdootSection.tsx
@@ -13,13 +13,19 @@ const UpdootSection: FC<UpdootSectionProps> = ({ post }) => {
   return (
     <Stack mr='4'>
       <IconButton
-        onClick={async () => await vote({ postId: post.id, value: 1 })}
+        colorScheme={post.voteStatus === 1 ? "green" : undefined}
+        onClick={async () => {
+          if (post.voteStatus !== 1) await vote({ postId: post.id, value: 1 });
+        }}
         icon={<ChevronUpIcon boxSize='24px' />}
         aria-label='Upvote'
       />
       <Text textAlign='center'>{post.points}</Text>
       <IconButton
-        onClick={async () => await vote({ postId: post.id, value: -1 })}
+        colorScheme={post.voteStatus === -1 ? "red" : undefined}
+        onClick={async () => {
+          if (post.voteStatus !== -1) await vote({ postId: post.id, value: -1 });
+        }}
         icon={<ChevronDownIcon boxSize='24px' />}
         aria-label='Downvote'
       />
