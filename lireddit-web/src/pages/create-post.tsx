b556547c17efa95c4070a110df4fc322de56dd8c
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-02-13 19:27:13 +0100

    FE: Invalidate posts when creating one

--- lireddit-web/src/pages/create-post.tsx
+++ lireddit-web/src/pages/create-post.tsx
@@ -1,13 +1,11 @@
 import { Box, Button } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import { NextPage } from "next";
-import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
 import React from "react";
 import { InputField } from "../components/InputField";
 import { Layout } from "../components/Layout";
 import { useCreatePostMutation } from "../generated/graphql";
-import { createUrqlClient } from "../utils/createUrqlClient";
 import { useIsAuth } from "../utils/useIsAuth";
 import { withApollo } from "../utils/withApollo";
 
@@ -22,7 +20,10 @@ const CreatePost: React.FC = ({}) => {
       <Formik
         initialValues={{ title: "", text: "" }}
         onSubmit={async (values) => {
-          const { errors } = await createPost({ variables: { input: values } });
+          const { errors } = await createPost({
+            variables: { input: values },
+            update: (cache) => cache.evict({ fieldName: "posts" }),
+          });
           if (errors?.[0]?.message.includes("not authenticated")) {
             router.push("/login");
           } else {
