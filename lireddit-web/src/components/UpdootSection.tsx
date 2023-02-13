abc351f4ee7ea25745ce277ec77e03a2bfe7c4e2
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-02-13 18:43:50 +0100

    FE: Up/down vote cache update with apollo

--- lireddit-web/src/components/UpdootSection.tsx
+++ lireddit-web/src/components/UpdootSection.tsx
@@ -1,21 +1,56 @@
+import { ApolloCache } from "@apollo/client";
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import { IconButton, Stack, Text } from "@chakra-ui/react";
+import gql from "graphql-tag";
 import { FC } from "react";
-import { PostSnippetFragment, useVoteMutation } from "../generated/graphql";
+import { Post, PostSnippetFragment, useVoteMutation } from "../generated/graphql";
 
 interface UpdootSectionProps {
   post: PostSnippetFragment;
 }
 
+const updateAfterVote = (value: number, postId: number, cache: ApolloCache<any>) => {
+  const data = cache.readFragment<Pick<Post, "id" | "points" | "voteStatus">>({
+    id: "Post:" + postId,
+    fragment: gql`
+      fragment _ on Post {
+        id
+        points
+        voteStatus
+      }
+    `,
+    variables: { id: postId },
+  });
+
+  if (data) {
+    if (data.voteStatus === value) return;
+    const newPoints = data.points + (data.voteStatus ? 2 : 1) * value;
+    cache.writeFragment({
+      id: "Post:" + postId,
+      fragment: gql`
+        fragment __ on Post {
+          points
+          voteStatus
+        }
+      `,
+      data: { id: postId, points: newPoints, voteStatus: value },
+    });
+  }
+};
+
 const UpdootSection: FC<UpdootSectionProps> = ({ post }) => {
-  const [, vote] = useVoteMutation();
+  const [vote] = useVoteMutation();
 
   return (
     <Stack mr='4'>
       <IconButton
         colorScheme={post.voteStatus === 1 ? "green" : undefined}
         onClick={async () => {
-          if (post.voteStatus !== 1) await vote({ postId: post.id, value: 1 });
+          if (post.voteStatus !== 1)
+            await vote({
+              variables: { postId: post.id, value: 1 },
+              update: (cache) => updateAfterVote(1, post.id, cache),
+            });
         }}
         icon={<ChevronUpIcon boxSize='24px' />}
         aria-label='Upvote'
@@ -24,7 +59,11 @@ const UpdootSection: FC<UpdootSectionProps> = ({ post }) => {
       <IconButton
         colorScheme={post.voteStatus === -1 ? "red" : undefined}
         onClick={async () => {
-          if (post.voteStatus !== -1) await vote({ postId: post.id, value: -1 });
+          if (post.voteStatus !== -1)
+            await vote({
+              variables: { postId: post.id, value: -1 },
+              update: (cache) => updateAfterVote(-1, post.id, cache),
+            });
         }}
         icon={<ChevronDownIcon boxSize='24px' />}
         aria-label='Downvote'
