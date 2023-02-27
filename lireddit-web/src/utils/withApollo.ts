35ec088717b0972aebb5ca9dfe0c9d876aac9c46
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-02-27 21:31:18 +0100

    FE: withApollo util

--- lireddit-web/src/utils/withApollo.ts
+++ lireddit-web/src/utils/withApollo.ts
@@ -1,25 +1,30 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { withApollo as createWithApollo } from "next-apollo";
+import { createWithApollo } from "../utils/createWithApollo";
 import { PaginatedPosts } from "../generated/graphql";
+import { NextPageContext } from "next";
 
-export const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          posts: {
-            keyArgs: [],
-            merge(existing: PaginatedPosts | undefined, incoming: PaginatedPosts) {
-              console.log(existing, incoming);
-              return { ...incoming, posts: [...(existing?.posts || []), ...incoming.posts] };
+export const createClient = (ctx: NextPageContext) =>
+  new ApolloClient({
+    uri: "http://localhost:4000/graphql",
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            posts: {
+              keyArgs: [],
+              merge(existing: PaginatedPosts | undefined, incoming: PaginatedPosts) {
+                console.log(existing, incoming);
+                return { ...incoming, posts: [...(existing?.posts || []), ...incoming.posts] };
+              },
             },
           },
         },
       },
+    }),
+    credentials: "include",
+    headers: {
+      cookie: (typeof window === "undefined" ? ctx?.req?.headers.cookie : undefined) || "",
     },
-  }),
-  credentials: "include",
-});
+  });
 
-export const withApollo = createWithApollo(client);
+export const withApollo = createWithApollo(createClient);
